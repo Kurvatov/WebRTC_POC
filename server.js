c609c0086f2c87e77f577d13db9f3d803c14e74c
Kurvatov <stephanemonteiro1999@outlook.com>
2023-06-09 12:39:22 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+// Handle socket connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Handle signaling events
+  socket.on('offer', (data) => {
+    socket.broadcast.emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.broadcast.emit('answer', data);
+  });
+
+  socket.on('ice-candidate', (data) => {
+    socket.broadcast.emit('ice-candidate', data);
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
