12be654497e0bf7f08ab8a9a3b058caaeebca920
Kurvatov <stephanemonteiro1999@outlook.com>
2023-06-12 15:58:22 +0100

    Added camera a mic features and improved the layout

---
+++ public/main.js
@@ -0,0 +1,129 @@
+// Get the room ID from the URL query parameters
+const urlParams = new URLSearchParams(window.location.search);
+const roomId = urlParams.get("roomId");
+
+// Connect to the Socket.IO server
+const socket = io();
+let isCameraEnabled = true;
+let isAudioEnabled = true;
+// Join the room with the specified room ID
+socket.emit("join-room", roomId);
+
+// Get the local video stream
+navigator.mediaDevices
+  .getUserMedia({ video: true, audio: true })
+  .then((stream) => {
+    const localVideo = document.getElementById("localVideo");
+    localVideo.srcObject = stream;
+
+    // Create a new WebRTC peer connection
+    const peerConnection = new RTCPeerConnection();
+
+    // Add the local stream to the peer connection
+    stream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track, stream);
+    });
+
+    // Listen for ICE candidates and send them to the other peer
+    peerConnection.addEventListener("icecandidate", (event) => {
+      if (event.candidate) {
+        socket.emit("ice-candidate", event.candidate);
+      }
+    });
+
+    // Listen for remote tracks and add them to the remote video element
+    peerConnection.addEventListener("track", (event) => {
+      const remoteVideo = document.getElementById("remoteVideo");
+      if (!remoteVideo.srcObject) {
+        remoteVideo.srcObject = new MediaStream();
+      }
+      remoteVideo.srcObject.addTrack(event.track);
+    });
+
+    // Send the offer to the other peer
+    peerConnection
+      .createOffer()
+      .then((offer) => {
+        socket.emit("offer", offer);
+        return peerConnection.setLocalDescription(offer);
+      })
+      .catch((error) => {
+        console.error("Error creating offer:", error);
+      });
+
+    // Handle the received offer
+    socket.on("offer", (offer) => {
+      peerConnection
+        .setRemoteDescription(offer)
+        .then(() => {
+          return peerConnection.createAnswer();
+        })
+        .then((answer) => {
+          socket.emit("answer", answer);
+          return peerConnection.setLocalDescription(answer);
+        })
+        .catch((error) => {
+          console.error("Error creating answer:", error);
+        });
+    });
+
+    // Handle the received answer
+    socket.on("answer", (answer) => {
+      peerConnection.setRemoteDescription(answer).catch((error) => {
+        console.error("Error setting remote description:", error);
+      });
+    });
+
+    // Handle the received ICE candidate
+    socket.on("ice-candidate", (candidate) => {
+      peerConnection.addIceCandidate(candidate).catch((error) => {
+        console.error("Error adding ICE candidate:", error);
+      });
+    });
+
+    // Disable or enable the camera
+    const disableCameraButton = document.getElementById(
+      "disableCameraButton"
+    );
+    disableCameraButton.addEventListener("click", () => {
+      if (isCameraEnabled) {
+        stream.getTracks().forEach((track) => {
+          track.enabled = false;
+        });
+        disableCameraButton.querySelector('img').src = './disable-camera.svg';
+      } else {
+        stream.getTracks().forEach((track) => {
+          track.enabled = true;
+        });
+        disableCameraButton.querySelector('img').src = './enable-camera.svg';
+      }
+      isCameraEnabled = !isCameraEnabled;
+    });
+    // Mute/unmute the audio
+    const muteButton = document.getElementById("muteButton");
+    muteButton.addEventListener("click", () => {
+      if (isAudioEnabled) {
+        stream.getAudioTracks().forEach((track) => {
+          track.enabled = false;
+        });
+        // muteButton.textContent = "Unmute";
+        muteButton.querySelector('img').src = './mute.svg';
+      } else {
+        stream.getAudioTracks().forEach((track) => {
+          track.enabled = true;
+        });
+        // muteButton.textContent = "Mute";
+        muteButton.querySelector('img').src = './unmute.svg';
+      }
+      isAudioEnabled = !isAudioEnabled;
+    });
+  })
+  .catch((error) => {
+    console.error("Error accessing media devices:", error);
+  });
+
+// Handle user-connected event
+socket.on("user-connected", (userId) => {});
+
+// Handle user-disconnected event
+socket.on("user-disconnected", (userId) => {});
--- server.js
+++ server.js
@@ -6,26 +6,45 @@ const io = require('socket.io')(http);
 // Serve static files
 app.use(express.static(__dirname + '/public'));
 
+// Store connected users and their rooms
+const users = {};
+
 // Handle socket connections
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Handle signaling events
+  // Handle joining a room
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    users[socket.id] = roomId;
+    console.log(`User joined room: ${roomId}`);
+
+    // Emit a 'user-connected' event to inform other users in the room
+    socket.to(roomId).emit('user-connected', socket.id);
+  });
+
+  // Handle signaling events within a room
   socket.on('offer', (data) => {
-    socket.broadcast.emit('offer', data);
+    const roomId = users[socket.id];
+    socket.to(roomId).emit('offer', data);
   });
 
   socket.on('answer', (data) => {
-    socket.broadcast.emit('answer', data);
+    const roomId = users[socket.id];
+    socket.to(roomId).emit('answer', data);
   });
 
   socket.on('ice-candidate', (data) => {
-    socket.broadcast.emit('ice-candidate', data);
+    const roomId = users[socket.id];
+    socket.to(roomId).emit('ice-candidate', data);
   });
 
   // Handle disconnection
   socket.on('disconnect', () => {
     console.log('A user disconnected');
+    const roomId = users[socket.id];
+    delete users[socket.id];
+    socket.to(roomId).emit('user-disconnected', socket.id);
   });
 });
 
